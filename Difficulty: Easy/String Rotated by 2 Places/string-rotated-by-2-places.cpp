f987931237048d88a6db29030c72af539199c3a3
Shreya singh <shryaon11@gmail.com>
2025-09-02 01:06:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.cpp
@@ -0,0 +1,56 @@
+class Solution {
+  public:
+  
+    void rotateclockwise(string& s)
+    {
+        char c = s[s.size()-1];
+        int index = s.size() - 2;
+        
+        while(index >=0)
+        {
+            s[index + 1] = s[index];
+            index--;
+        }
+        s[0] = c;
+    }
+    
+    void rotateanticlockwise(string& s)
+    {
+        char c = s[0];
+        int index = 1;
+        
+        while(index < s.size())
+        {
+            s[index - 1] = s[index];
+            index++;
+        }
+        s[s.size()-1] = c;
+    }
+  
+  
+    bool isRotated(string& s1, string& s2) {
+        // code here
+        if(s1.size() != s2.size())
+        return 0;
+        
+        string clockwise = s1;
+        rotateclockwise(clockwise);
+        rotateclockwise(clockwise);
+        
+        if(clockwise == s2)
+        return 1;
+        
+        string anticlockwise = s1;
+        rotateanticlockwise(anticlockwise);
+        rotateanticlockwise(anticlockwise);
+        
+        if(anticlockwise == s2)
+        return 1;
+        
+        
+        return 0;
+        
+    }
+    
+    
+};
